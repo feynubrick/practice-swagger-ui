bab0a90af52d21a889d0b159f86cbd0ff158c9e7
Seung-Yoon Oh <feynubrick@gmail.com>
2022-04-13 14:30:33 +0900

    swagger ui 설정 파일 자동 빌드되도록 추가

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 
 const app = express();
 const port = 3000;
 
+const swaggerDoc = YAML.load('./api-doc.yaml');
+
 app.get('/', (req, res) => {
   res.send('OK');
 });
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+npx swagger-cli bundle ./swagger/swagger.yaml --outfile api-doc.yaml --type yaml
