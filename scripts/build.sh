12913e5f28783c06470d658c21e08b658f74e537
Seung-Yoon Oh <feynubrick@gmail.com>
2022-04-15 16:03:47 +0900

    swagger/build 디렉토리가 없어도 build 스크립트가 제대로 동작하도록 만들기

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 SRC="./swagger/src/swagger.yaml"
-TARGET="./swagger/build/swagger.yaml"
+TARGET_DIR="./swagger/build"
+TARGET="$TARGET_DIR/swagger.yaml"
+
+mkdir -p $TARGET_DIR
 npx swagger-cli bundle $SRC --outfile $TARGET --type yaml
